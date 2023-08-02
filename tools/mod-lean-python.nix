b2a20a7a57e99d3105bde8210d799f1e32fa15ab
Derek Kulinski <d@kulinski.us>
2023-08-02 16:34:03 -0700

    adjustment to lean-python to not break with cryptography package

--- tools/mod-lean-python.nix
+++ tools/mod-lean-python.nix
@@ -9,7 +9,7 @@
           package = mkOption {
             type = types.package;
             description = "python package";
-            example = "python38";
+            example = "python311";
           };
           bzip2 = mkOption { type = types.bool; default = false; };
           # you might need it on darwin for _scproxy module
@@ -35,6 +35,7 @@
           rebuild_bytecode = mkOption { type = types.bool; default = false; };
           strip_bytecode = mkOption { type = types.bool; default = true; };
           include_site_customize = mkOption { type = types.bool; default = false; };
+          lto = mkOption { type = types.bool; default = true; };
           enable_optimizations = mkOption { type = types.bool; default = false; };
           strip_debug_symbols = mkOption { type = types.bool; default = true; };
         };
@@ -51,37 +52,43 @@
 
   config = let
     lean_python = config.lean_python;
-  in {
     out_lean_python = with lib; if (!config.lean_python.enable || is_shell)
-    then lean_python.package
-    else (lean_python.package.override ({
-      mimetypesSupport = lean_python.mimetypes_support;
-      x11Support = lean_python.x11_support;
-      stripConfig = lean_python.strip_config;
-      stripIdlelib = lean_python.strip_idlelib;
-      stripTests = lean_python.strip_tests;
-      stripTkinter = lean_python.strip_tkinter;
-      rebuildBytecode = lean_python.rebuild_bytecode;
-      stripBytecode = lean_python.strip_bytecode;
-      includeSiteCustomize = lean_python.include_site_customize;
-      enableOptimizations = lean_python.enable_optimizations && !pkgs.stdenv.cc.isClang;
-    }
-    // optionalAttrs (!lean_python.bzip2) { bzip2 = null; }
-    // optionalAttrs (!lean_python.configd) { configd = null; }
-    // optionalAttrs (!lean_python.expat) { expat = null; }
-    // optionalAttrs (!lean_python.libffi) { libffi = pkgs.libffiBoot; }
-    // optionalAttrs (!lean_python.gdbm) { gdbm = null; }
-    // optionalAttrs (!lean_python.xz) { xz = null; }
-    // optionalAttrs (!lean_python.ncurses) { ncurses = null; }
-    // optionalAttrs (!lean_python.openssl) { openssl = null; openssl_legacy = null; }
-    // optionalAttrs (!lean_python.readline) { readline = null; }
-    // optionalAttrs (!lean_python.sqlite) { sqlite = null; }
-    // optionalAttrs (!lean_python.zlib) { zlib = null; }
-    // optionalAttrs (!lean_python.tzdata) { tzdata = null; }
-    )).overrideAttrs (old: {
-      preConfigure = (old.preConfigure or "") + lib.optionalString (lean_python.strip_debug_symbols && !pkgs.stdenv.buildPlatform.isDarwin) ''
-        export NIX_LDFLAGS+=" --strip-all"
-      '';
-    });
+      then lean_python.package
+      else (lean_python.package.override ({
+        self = out_lean_python;
+        mimetypesSupport = lean_python.mimetypes_support;
+        x11Support = lean_python.x11_support;
+        stripConfig = lean_python.strip_config;
+        stripIdlelib = lean_python.strip_idlelib;
+        stripTests = lean_python.strip_tests;
+        stripTkinter = lean_python.strip_tkinter;
+        enableLTO = lean_python.lto && (pkgs.stdenv.is64bit && pkgs.stdenv.isLinux);
+        rebuildBytecode = lean_python.rebuild_bytecode;
+        stripBytecode = lean_python.strip_bytecode;
+        includeSiteCustomize = lean_python.include_site_customize;
+        enableOptimizations = lean_python.enable_optimizations && !pkgs.stdenv.cc.isClang;
+      }
+      // optionalAttrs (!lean_python.bzip2) { bzip2 = null; }
+      // optionalAttrs (!lean_python.configd) { configd = null; }
+      // optionalAttrs (!lean_python.expat) { expat = null; }
+      // optionalAttrs (!lean_python.libffi) { libffi = pkgs.libffiBoot; }
+      // optionalAttrs (!lean_python.gdbm) { gdbm = null; }
+      // optionalAttrs (!lean_python.xz) { xz = null; }
+      // optionalAttrs (!lean_python.ncurses) { ncurses = null; }
+      // optionalAttrs (!lean_python.openssl) { openssl = null; openssl_legacy = null; }
+      // optionalAttrs (!lean_python.readline) { readline = null; }
+      // optionalAttrs (!lean_python.sqlite) { sqlite = null; }
+      // optionalAttrs (!lean_python.zlib) { zlib = null; }
+      // optionalAttrs (!lean_python.tzdata) { tzdata = null; }
+      ));
+#      .overrideAttrs (old: {
+#        strictDeps = true;
+#        pname = "lean-python3";
+#        preConfigure = (old.preConfigure or "") + lib.optionalString (lean_python.strip_debug_symbols && !pkgs.stdenv.buildPlatform.isDarwin) ''
+#          export NIX_LDFLAGS+=" --strip-all"
+#        '';
+#      });
+  in {
+    inherit out_lean_python;
   };
 }
