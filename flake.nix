e21fb8eb9955e56493c1db647c4315b33c18b200
Derek Kulinski <d@kulinski.us>
2023-08-23 01:06:51 -0700

    Updated description

--- flake.nix
+++ flake.nix
@@ -1,5 +1,5 @@
 {
-  description = "nix-cde";
+  description = "Nix-CDE (Nix-based Common Development Envrionemnt) provides a reproducible development environment that abstracts away Nix rough edges through the use of NixOS modules.";
 
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/nixpkgs-unstable";
